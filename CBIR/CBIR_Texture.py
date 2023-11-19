83a8c1201cb5f1a8d0980e24b6a5928baf352308
hiirr <zahiradina2303@gmail.com>
2023-11-19 15:07:51 +0700

    feat: add fix in texture

--- CBIR/CBIR_Texture.py
+++ CBIR/CBIR_Texture.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 def to_gray(img):
-    r, g, b = img[..., 0], img[..., 1], img[..., 2]
+    r, g, b = img[:, :, 0], img[:, :, 1], img[:, :, 2]
     return 0.299 * r + 0.587 * g + 0.114 * b
 
 def process_chedec(image):
